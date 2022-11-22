a88d03055c231c52f4cf580b0781411db4f7de8c
Jonatoru <jetrence95@gmail.com>
2022-11-22 23:11:42 +0500

    Решил задание 3.

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,21 @@ public class Main {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке.");
             }
         }
+
+        System.out.println("Задача 3");
+
+        int year = 2022;
+        int leapYear = 4;
+        int noLeapYear = 100;
+        int leapYear400 = 400;
+        if (year % noLeapYear == 0 && year % leapYear400 == 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year % leapYear == 0 && year % noLeapYear != 0) {
+            System.out.println(year + " год является високосным.");
+        } else if (year % noLeapYear == 0) {
+            System.out.println(year + " год не является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
     }
 }
